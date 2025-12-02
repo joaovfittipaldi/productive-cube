5c556eb94d917db51bddb1ba62143027067cb1ae
Nerebo <andregoesf@gmail.com>
2025-12-02 09:31:13 -0300

    Patch de Cors

--- flask_server/app/views.py
+++ flask_server/app/views.py
@@ -1,17 +1,20 @@
 from flask import Blueprint
 from flask import json
-from app import database_manager
+from flask_cors import cross_origin
+from app import database_manager    
 
 bp = Blueprint("endpoints", __name__)
 
 # Definindo endpoints
 
 @bp.route("/sessoes_completas")
+@cross_origin(origins=["http://localhost:3000"])
 def teste_view():
     # Retorno {"sessoes_nao_completas": X}
     return database_manager.tempo_terminou()
 
 @bp.route("/dia")
+@cross_origin(origins=["http://localhost:3000"])
 def meta_diaria():
     # Retorno: {"total_diario": X}
 
@@ -19,6 +22,7 @@ def meta_diaria():
     return meta_diaria
 
 @bp.route("/semana")
+@cross_origin(origins=["http://localhost:3000"])
 def meta_semanal():
     # Retorno: {
     # "meta_semanal": X
@@ -28,6 +32,7 @@ def meta_semanal():
     return database_manager.total_semana()
 
 @bp.route("/mes")
+@cross_origin(origins=["http://localhost:3000"])
 def meta_mensal():
     # Retorno: {
     # "meta_mensal": X
@@ -37,5 +42,6 @@ def meta_mensal():
     return database_manager.meta_mensal()
 
 @bp.route("/dashboard")
+@cross_origin(origins=["http://localhost:3000"])
 def dashboard_semanal():
     return database_manager.desempenho_semanal()
